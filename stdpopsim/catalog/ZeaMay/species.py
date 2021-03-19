86d28fac15efc93d33076d9504e530b9412fe0fe
Gregor Gorjanc <gregor.gorjanc@gmail.com>
2021-03-19 21:26:50 +0000

    Black

--- stdpopsim/catalog/ZeaMay/species.py
+++ stdpopsim/catalog/ZeaMay/species.py
@@ -2,7 +2,6 @@ import stdpopsim
 
 from . import genome_data
 
-# TODO
 _ToDOEtAl = stdpopsim.Citation(
     author="TODO",
     year=-1,
@@ -14,7 +13,13 @@ _ToDOEtAl = stdpopsim.Citation(
 # stdpopsim.CiteReason.GEN_TIME
 # stdpopsim.CiteReason.POP_SIZE
 # stdpopsim.CiteReason.REC_RATE
-# stdpopsim.CiteReason.MUT_RATE
+
+_ClarkEtAl = stdpopsim.Citation(
+    author="Clark et al.",
+    year=2005,
+    doi="https://doi.org/10.1093/molbev/msi228",
+    reasons={stdpopsim.CiteReason.MUT_RATE},
+)
 
 # TODO
 # [The following are notes for implementers and should be deleted
@@ -42,37 +47,20 @@ _recombination_rate = {
     "Mt": 0,
 }
 
-# TODO
-# [The following are notes for implementers and should be deleted
-#  once the mutation rates have been inserted]
-# This is the per-chromosome mutation rate, typically the mean
-# rate along the chromosome. If per chromosome rates are not available,
-# the same value should be used for each chromosome. In this case,
-# please use a variable to store this value, rather than repeating
-# the same numerical constant, e.g.
-# _mutation_rate = {
-#    1: _overall_rate,
-#    2: _overall_rate,
-#    ...
-# Values in this dictionary are set to -1 by default, so you have
-# to update each one. These should be derived from the most reliable
-# data and how they were obtained should be documented here.
-# The appropriate citation must be added to the list of
-# mutation_rate_citations in the Genome object.
-
+_overall_rate = 3.3e-8
 _mutation_rate = {
-    "1": -1,
-    "2": -1,
-    "3": -1,
-    "4": -1,
-    "5": -1,
-    "6": -1,
-    "7": -1,
-    "8": -1,
-    "9": -1,
-    "10": -1,
-    "Pt": -1,
-    "Mt": -1,
+    "1": _overall_rate,
+    "2": _overall_rate,
+    "3": _overall_rate,
+    "4": _overall_rate,
+    "5": _overall_rate,
+    "6": _overall_rate,
+    "7": _overall_rate,
+    "8": _overall_rate,
+    "9": _overall_rate,
+    "10": _overall_rate,
+    "Pt": _overall_rate,
+    "Mt": _overall_rate,
 }
 
 _genome = stdpopsim.Genome.from_data(
@@ -80,7 +68,7 @@ _genome = stdpopsim.Genome.from_data(
     recombination_rate=_recombination_rate,
     mutation_rate=_mutation_rate,
     # TODO citations
-    citations=[],
+    citations=[_ClarkEtAl],
 )
 
 _species = stdpopsim.Species(
